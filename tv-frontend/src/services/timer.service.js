6d93990c1efcee98f6a627c0bd8ec13191e3d47c
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-09-08 10:54:48 +0530

    remove timer service

--- tv-frontend/src/services/timer.service.js
+++ tv-frontend/src/services/timer.service.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+// import axios from 'axios';
+// import { PROD_API_URL } from "../constants";
 
 // const apiClient = axios.create({
 //   baseURL: 'http://localhost:5000/api',
@@ -16,14 +17,14 @@ import axios from 'axios';
 //     });
 // };
 
-const baseURL = 'http://localhost:5000';
+// const API_URL = 'http://localhost:5000';
 
-export const fetchTime = async () => {
-  try {
-    const response = await axios.get(`${baseURL}/time`);
-    return response.data.fetchedTime;
-  } catch (error) {
-    console.error('Error fetching the number:', error);
-    throw error;
-  }
-};
+// export const fetchTime = async () => {
+//   try {
+//     const response = await axios.get(`${PROD_API_URL}/api/time`);
+//     return response.data.fetchedTime;
+//   } catch (error) {
+//     console.error('Error fetching time:', error);
+//     throw error;
+//   }
+// };
