68e09e459dd93b536cf1c12a2a237cce24cde6b7
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-09-08 10:53:00 +0530

    updated theme

--- admin-frontend/src/index.js
+++ admin-frontend/src/index.js
@@ -7,11 +7,29 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Define your custom theme here
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2c3e50", // Custom primary color
+    },
+    secondary: {
+      main: "#95a5a6", // Custom secondary color
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </PersistGate>
   </Provider>
 );
