54f4b060808d823dac9af36aea82a06d0e2878f2
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-09-28 00:17:48 +0530

    score UI

--- tv-frontend/src/components/Score.js
+++ tv-frontend/src/components/Score.js
@@ -1,7 +1,13 @@
 const Score = () => {
   return (
-    <div className="h-[40vh] m-0 bg-gray-300 text-[#ecf0f1] text-4xl flex justify-center items-center">
-      Score: 135/4
+    <div className="h-[40vh] m-0 bg-[#2c3e50] text-[#ecf0f1] flex justify-evenly items-center">
+      <div className="flex flex-col items-center mx-8">
+        <div className="font-bold text-[12rem] leading-none">135/4</div>
+      </div>
+      <div className="flex flex-col items-center mx-8">
+        <div className="text-6xl font-light uppercase">Overs</div>
+        <div className="font-bold text-[8rem] leading-none">1.5</div>
+      </div>
     </div>
   );
 };
