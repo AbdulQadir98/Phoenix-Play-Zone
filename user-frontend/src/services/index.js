710db0cd942ad48b94fc9b0b6057a2353a7a40be
AbdulQadir98 <abdul.qadirv98@gmail.com>
2024-10-09 05:29:50 +0530

    added cric app

---
+++ user-frontend/src/services/index.js
@@ -0,0 +1,41 @@
+import { PROD_API_URL } from "../constants";
+import axios from "axios";
+
+export const startMatch = async (cid) => {
+    try {
+        const response = await axios.post(PROD_API_URL + `/start-match/${cid}`, {}, {
+            timeout: 4000 // 4 seconds timeout
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error starting match:", error.message);
+        throw error;
+    }
+};
+
+export const resetMatch = async (cid) => {
+    try {
+        const response = await axios.post(PROD_API_URL + `/reset-match/${cid}`, {}, {
+            timeout: 4000 // 4 seconds timeout
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error reseting match:", error.message);
+        throw error;
+    }
+};
+
+export const updateScore = async (cid, increment, isWicket) => {
+    try {
+        const response = await axios.post(`${PROD_API_URL}/update-score/${cid}`, {
+            increment,
+            isWicket
+        }, {
+            timeout: 4000 // 4 seconds timeout
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error updating score", error.message);
+        throw error;
+    }
+};
